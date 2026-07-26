d519853935488e655086eef6458491d946509800
luoshuang435 <luoshuang435@gmail.com>
2026-07-26 19:18:05 +0800

    Create exercise_1_3.scm

---
+++ chapter_1/exercise_1_3.scm
@@ -0,0 +1,8 @@
+(define (square x) (* x x))
+
+(define (sum-square x y) (+ (square x) (square y)))
+
+(define (fun x y z)
+  (cond ((and (<= x y) (<= x z)) (sum-square y z))
+        ((and (<= y x) (<= y z)) (sum-square x z))
+        (else                    (sum-square x y))))
